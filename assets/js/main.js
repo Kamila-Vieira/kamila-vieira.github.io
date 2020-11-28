afbfd8462a0332db7eca22ef8ddd8ded6a154575
Kamila Almeida <kamila.almeida@acertonline.local>
2020-11-28 20:44:19 -0300

    Melhorias no header/logo/menu responsivo

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,12 @@ const menuResponsive = document.querySelector('.menu-responsive')
 function toogleResponsiveMenu(){
   let navigationBarResponsive = document.querySelector('#nav-bar-responsive');
   if (navigationBarResponsive.style.display === "none") {
+    menuResponsive.innerHTML = "X"
     menuResponsive.style.transform = "rotate(-90deg)";
     navigationBarResponsive.style.display = "block";
   } else {
-    menuResponsive.style.transform = "rotate(0deg)"
+    menuResponsive.innerHTML = "&#9776;"
+    menuResponsive.style.transform = "rotate(0deg)";
     navigationBarResponsive.style.display = "none";
   }
 }
