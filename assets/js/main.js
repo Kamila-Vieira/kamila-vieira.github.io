ef7b17180fd7ddeccd3068c09c946f3b0b835cc3
Kamila Almeida <kamila.almeida@acertonline.local>
2020-12-09 14:44:02 -0300

    Finalização do menu responsivo

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,21 +2,13 @@ const menuResponsive = document.querySelector('.menu-responsive');
 const firstBarMenu = menuResponsive.querySelector('.first-bar-menu');
 const secondBarMenu = menuResponsive.querySelector('.second-bar-menu');
 const thirdBarMenu = menuResponsive.querySelector('.third-bar-menu');
-const navigationBarResponsive = document.querySelector('#nav-responsive');
+const navigationBarResponsive = document.querySelector('.nav-responsive');
 
-function toogleResponsiveMenu(event){
-  event.preventDefault();
-  if(navigationBarResponsive.style.display = "none"){
-    navigationBarResponsive.style.display = "block";
-  }else{
-    navigationBarResponsive.style.display = "none";
-  }
- /*  if(navigationBarResponsive.style.display = 'block'){
-    navigationBarResponsive.style.display = 'none';
-  } */
+function toogleResponsiveMenu(){
   firstBarMenu.classList.toggle('first-bar-menu-after');
   secondBarMenu.classList.toggle('second-bar-menu-after');
   thirdBarMenu.classList.toggle('third-bar-menu-after');
+  navigationBarResponsive.classList.toggle('nav-bar-menu-after');
 }
 
 menuResponsive.onclick = toogleResponsiveMenu;
