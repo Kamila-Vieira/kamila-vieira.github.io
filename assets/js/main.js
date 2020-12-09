90039aa02125c9d76ed6c13e1a67b3e0afa5b430
Kamila Almeida <kamila.almeida@acertonline.local>
2020-12-09 11:01:32 -0300

    Melhorias no menu responsivo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,22 @@
-const menuResponsive = document.querySelector('.menu-responsive')
+const menuResponsive = document.querySelector('.menu-responsive');
+const firstBarMenu = menuResponsive.querySelector('.first-bar-menu');
+const secondBarMenu = menuResponsive.querySelector('.second-bar-menu');
+const thirdBarMenu = menuResponsive.querySelector('.third-bar-menu');
+const navigationBarResponsive = document.querySelector('#nav-responsive');
 
-function toogleResponsiveMenu(){
-  let navigationBarResponsive = document.querySelector('#nav-bar-responsive');
-  if (navigationBarResponsive.style.display === "none") {
-    menuResponsive.innerHTML = "X"
-    menuResponsive.style.transform = "rotate(-90deg)";
+function toogleResponsiveMenu(event){
+  event.preventDefault();
+  if(navigationBarResponsive.style.display = "none"){
     navigationBarResponsive.style.display = "block";
-  } else {
-    menuResponsive.innerHTML = "&#9776;"
-    menuResponsive.style.transform = "rotate(0deg)";
+  }else{
     navigationBarResponsive.style.display = "none";
   }
+ /*  if(navigationBarResponsive.style.display = 'block'){
+    navigationBarResponsive.style.display = 'none';
+  } */
+  firstBarMenu.classList.toggle('first-bar-menu-after');
+  secondBarMenu.classList.toggle('second-bar-menu-after');
+  thirdBarMenu.classList.toggle('third-bar-menu-after');
 }
 
 menuResponsive.onclick = toogleResponsiveMenu;
